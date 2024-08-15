650060c855d226cda8db0e542d7d6147b4e28427
Волочаев Даниил <volochaev@techart.ru>
2024-08-15 15:06:41 +0300

    Обновили анимацию

--- t.js
+++ t.js
@@ -1,5 +1,4 @@
 const FRACTION = 220;
-
 /**
  * Тротлим анимацию на 60 кадров в секунду
  * 
@@ -24,33 +23,11 @@ document.addEventListener('DOMContentLoaded', () => {
 	const lines = {
 		left: [[300, 0], [0, 1000]],
 		right: [[700, 0],[1000, 1000]],
-		up: [[700, 0],[300, 0]],
-		down: [[1000, 1000], [0, 1000]],
+		first_middle: [[300 + 133.3333333, 0], [333.333333, 1000]],
+		second_middle: [[300 + 266.66666, 0], [666.666666, 1000]],
+		up: [[300, 0],[700, 0]],
+		down: [[0, 1000], [1000, 1000]],
 	}
-	
-	/**
-	 * Все цифры предрасчитаны чтобы повысить производительность, ради христа не трогайте их, если не понимаете что делаете
-	 * 
-	 * Если всетаки надо поменяь то стройте массив нодов на линии и считайте оттуда. Тут использовалась точность в 9000 нодов на линию
-	 */
-	const startMoveValues = [
-		{
-			left: [262.5, 125],
-			right: [737.5, 125],
-		},
-		{
-			left: [200, 333.3333333333333],
-			right: [800, 333.3333333333333],
-		},
-		{
-			left: [112.5, 625],
-			right: [887.5, 625],
-		},
-		{
-			left: [0, 1000],
-			right: [1000, 1000],
-		},
-	];
 
 	const precalculatePoints = (start, end, segments) => {
 		const xDelta = (end[0] - start[0]) / segments;
@@ -65,35 +42,33 @@ document.addEventListener('DOMContentLoaded', () => {
 		return points.reverse();
 	};
 
-lines.left_route = precalculatePoints(lines.left[0], lines.left[1], 9000);
-lines.right_route = precalculatePoints(lines.right[0], lines.right[1], 9000);
-const routeLimit = Math.min(lines.left_route.length, lines.right_route.length);
-
-let indexes = [
-	Math.floor(routeLimit / 8) * 7,
-	Math.floor(routeLimit / 6) * 4,
-	Math.floor(routeLimit / 8) * 3,
-	0,
-];
-
-const startPoints = [
-	indexes[0],
-	indexes[1],
-	indexes[2],
-	indexes[3]
-];
-
-const f = Math.floor(routeLimit / 8) * 3;
-
-console.log([lines.left_route[f], lines.right_route[f]]);
-
-const endPoints = [
-	routeLimit,
-	indexes[0],
-	indexes[1],
-	indexes[2],
-];
+	lines.left_route = precalculatePoints(lines.left[0], lines.left[1], 9000);
+	lines.right_route = precalculatePoints(lines.right[0], lines.right[1], 9000);
+	lines.first_middle_route = precalculatePoints(lines.first_middle[0], lines.first_middle[1], 9000);
+	lines.second_middle_route = precalculatePoints(lines.second_middle[0], lines.second_middle[1], 9000);
+	const routeLimit = Math.min(lines.left_route.length, lines.right_route.length);
 	
+	const startMoveValues = [
+		{
+			left: lines.left_route[Math.floor(routeLimit / 8) * 7],
+			right: lines.right_route[Math.floor(routeLimit / 8) * 7],
+		},
+		{
+			left: lines.left_route[Math.floor(routeLimit / 6) * 4],
+			right: lines.right_route[Math.floor(routeLimit / 6) * 4],
+		},
+		{
+			left: lines.left_route[Math.floor(routeLimit / 8) * 3],
+			right: lines.right_route[Math.floor(routeLimit / 8) * 3],
+		},
+		{
+			left: lines.left_route[0],
+			right: lines.right_route[0],
+		},
+	];
+
+	let currentLoading = [];
+
 	let current = [
 		{
 			left: [startMoveValues[0].left[0], startMoveValues[0].left[1]],
@@ -221,15 +196,193 @@ const endPoints = [
 	let lastFrame = 0;
 	let dTime = 1;
 
-	/**
-	 * цикл анимации
-	 */
-	const animation = () => {
+	const LOADING_STATE = 'loading';
+	const PLAYING_STATE = 'playing';
+	const HIDING_STATE = 'hiding';
+	const STOPED_STATE = 'stoped';
+
+	let animationState = window.scrollY < 200 ? LOADING_STATE : HIDING_STATE;
+
+	const animationManager = () => {
 		if (lastFrame > 0 && performance.now() - lastFrame < TARGET_FRAME_TIME) {
-			requestAnimationFrame(animation);
+			requestAnimationFrame(animationManager);
+			return;
+		}
+		switch (animationState) {
+			case PLAYING_STATE: 
+				playingAnimation();
+				break;
+			case LOADING_STATE:
+				loadingAnimation();
+				break;
+			case HIDING_STATE:
+				hideAnimation();
+				break;
+			default:
+			case STOPED_STATE:
+				context.clearRect(0, 0, 1000, 1000);
+				break;
+		}
+
+		const frameTime = performance.now() - lastFrame;
+		dTime = frameTime / TARGET_FRAME_TIME;
+		lastFrame = performance.now();
+		requestAnimationFrame(animationManager);
+	}
+
+	let loadingIndex = 0;
+
+	const hideAnimation = () => {
+		context.clearRect(0, 0, 1000, 1000);
+		if (currentLoading.length === 0) {
 			return;
 		}
 
+		context.beginPath();
+		context.strokeStyle = '#FF0';
+		context.lineWidth = 2;
+
+		let lastLoading = false;
+		let lastLeft = false;
+		let lastRight = false;
+
+		Object.values(currentLoading).forEach((item) => {
+			Object.entries(item).forEach((entry) => {
+				if (!lastLoading) {
+					return;
+				}
+
+				const key = entry[0];
+				const value = entry[1];
+	
+				if (['left', 'right', 'up', 'down', 'first_middle', 'second_middle'].includes(key)) {
+					if (value[0] !== 0 && value[1] !== 0) {
+						context.moveTo(lastLoading[key][0], lastLoading[key][1]);
+						context.lineTo(value[0], value[1]);
+					}
+				}
+
+				if (key === 'left') {
+					lastLeft = [value[0], value[1]];
+				}
+
+				if (key === 'right') {
+					lastRight = [value[0], value[1]];
+				}
+			});
+			lastLoading = item;
+		});
+
+		if (currentLoading[0]) {
+			context.moveTo(currentLoading[0]['left'][0], currentLoading[0]['left'][1]);
+			context.lineTo(currentLoading[0]['right'][0], currentLoading[0]['right'][1]);
+		}
+
+		context.moveTo(lastLeft[0], lastLeft[1]);
+		context.lineTo(lastRight[0], lastRight[1]);
+
+		Object.values(startMoveValues).forEach((moveValue) => {
+			if (lastLeft[1] >= moveValue.left[1]) {
+				context.moveTo(moveValue.left[0], moveValue.right[1]);
+				context.lineTo(moveValue.right[0], moveValue.right[1]);
+			}
+		});
+
+		context.stroke();
+
+		currentLoading.splice(currentLoading.length - 3, 3);
+		loadingIndex -= 300;
+
+		if (loadingIndex <= 0) {
+			loadingIndex = 0;
+			animationState = STOPED_STATE;
+			canvas.dispatchEvent(new CustomEvent('hide_stoped'));
+		}
+	}
+	
+	const loadingAnimation = () => {
+		context.clearRect(0, 0, 1000, 1000);
+		document.body.style.position = 'fixed';
+		document.body.style.overflowY = 'scroll';
+		document.body.style.width = '100%';
+
+		context.beginPath();
+		context.strokeStyle = '#FF0';
+		context.lineWidth = 2;
+
+		let lastLoading = false;
+		let lastLeft = false;
+		let lastRight = false;
+
+		Object.values(currentLoading).forEach((item) => {
+			Object.entries(item).forEach((entry) => {
+				if (!lastLoading) {
+					return;
+				}
+
+				const key = entry[0];
+				const value = entry[1];
+	
+				if (['left', 'right', 'up', 'down', 'first_middle', 'second_middle'].includes(key)) {
+					if (value[0] !== 0 && value[1] !== 0) {
+						context.moveTo(lastLoading[key][0], lastLoading[key][1]);
+						context.lineTo(value[0], value[1]);
+					}
+				}
+
+				if (key === 'left') {
+					lastLeft = [value[0], value[1]];
+				}
+
+				if (key === 'right') {
+					lastRight = [value[0], value[1]];
+				}
+			});
+			lastLoading = item;
+		});
+
+		context.moveTo(lastLeft[0], lastLeft[1]);
+		context.lineTo(lastRight[0], lastRight[1]);
+
+		Object.values(startMoveValues).forEach((moveValue) => {
+			if (lastLeft[1] <= moveValue.left[1]) {
+				context.moveTo(moveValue.left[0], moveValue.right[1]);
+				context.lineTo(moveValue.right[0], moveValue.right[1]);
+			}
+		});
+
+		if (loadingIndex >= routeLimit) {
+			loadingIndex = routeLimit - 1;
+		}
+		
+		context.stroke();
+
+		for (let i = 0; i < 3; i++) {
+			currentLoading.push({
+				left: [lines.left_route[loadingIndex][0], lines.left_route[loadingIndex][1]],
+				right: [lines.right_route[loadingIndex][0], lines.right_route[loadingIndex][1]],
+				first_middle: [lines.first_middle_route[loadingIndex][0], lines.first_middle_route[loadingIndex][1]],
+				second_middle: [lines.second_middle_route[loadingIndex][0], lines.second_middle_route[loadingIndex][1]],
+				up: [0, 0],
+				down: [0, 0]
+			});
+
+			loadingIndex += 100;
+		}
+
+		if (loadingIndex >= routeLimit) {
+			animationState = PLAYING_STATE;
+			document.body.style.position = '';
+			document.body.style.overflowY = '';
+			document.body.style.width = '%';
+			canvas.dispatchEvent(new CustomEvent('loading_stoped'));
+		}
+	}
+
+	/**
+	 * цикл анимации
+	 */
+	const playingAnimation = () => {
 		context.clearRect(0,0, 1000, 1000);
 
 		context.beginPath();
@@ -241,13 +394,12 @@ const endPoints = [
 			const key = entry[0];
 			const value = entry[1];
 
-			if (['left', 'right', 'up', 'down'].includes(key)) {
+			if (['left', 'right', 'up', 'down', 'first_middle', 'second_middle'].includes(key)) {
 				context.moveTo(value[0][0], value[0][1]);
 				context.lineTo(value[1][0], value[1][1]);
 			}
 		});
 		context.stroke();
-
 		/**
 		 * рисуем анимированные линии
 		 */
@@ -288,11 +440,43 @@ const endPoints = [
 			}
 			return entry;
 		});
-
-		const frameTime = performance.now() - lastFrame;
-		dTime = frameTime / TARGET_FRAME_TIME;
-		lastFrame = performance.now();
-		requestAnimationFrame(animation);
 	};
-	requestAnimationFrame(animation);
+	requestAnimationFrame(animationManager);
+
+	let hideReversed = false;
+	let loadReversed = false;
+
+	window.addEventListener('scroll', (event) => {
+		if (window.scrollY > 0 && animationState === PLAYING_STATE) {
+			animationState = HIDING_STATE;
+			if (!hideReversed) {
+				currentLoading.reverse();
+				hideReversed = true;
+				loadReversed = false;
+			}
+
+		}
+
+		if (window.scrollY === 0) {
+			const callback = () => {
+				animationState = LOADING_STATE;
+				if (!loadReversed) {
+					currentLoading.reverse();
+					loadReversed = true;
+					hideReversed = false;
+				}
+			}
+
+			if (animationState === HIDING_STATE) {
+				canvas.addEventListener('hide_stoped', () => {
+					if (window.scrollY === 0) {
+						callback();
+					}
+					canvas.removeEventListener('hide_stoped', callback);
+				});
+			} else {
+				callback();
+			}
+		}
+	}, {passive: false});
 });
